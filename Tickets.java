78bc860682e62e05d84219b3cb1e8317536219a0
Taylor Wong <twong7@stuy.edu>
2017-04-20 22:04:03 -0400

    added accessor methods

--- Tickets.java
+++ Tickets.java
@@ -6,5 +6,28 @@ public class Ticket {
     private String custName;
     private String solution;
     private boolean solved;
+    
+    public int getVIPLevel() {
+        return VIPLevel;
+    }
+    
+    public int getID() {
+        return ID;   
+    }
+    
+    public String getProblem() {
+        return problem;   
+    }
 
+    public String getCustName() {
+        return custName;   
+    }
+    
+    public int getSolution() {
+        return solution;   
+    }
+    
+    public boolean isSolved() {
+        return solved;   
+    }
 }
