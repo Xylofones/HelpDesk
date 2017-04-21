1997dab9eb198f38ccc75664e710e67dac316a79
Taylor Wong <twong7@stuy.edu>
2017-04-21 10:44:06 -0400

    addTicket method

--- HelpDesk.java
+++ HelpDesk.java
@@ -3,4 +3,12 @@ public class HelpDesk {
     private ArrayPriorityQueue<Ticket> customers;
     private int currID;
     
+    public void addTicket(int VIPLevel, String problem, String custName) {
+    
+        Ticket newTicket = new Ticket(VIPLevel, currID, problem, custName);
+        customers.add(newTicket);
+        System.out.println(custName + "needs help!");
+        
+    }
+    
 }
