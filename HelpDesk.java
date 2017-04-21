9afbbb6770941e4234199e4c0bca7785d44d3647
Taylor Wong <twong7@stuy.edu>
2017-04-21 11:01:41 -0400

    add answers

--- HelpDesk.java
+++ HelpDesk.java
@@ -2,12 +2,19 @@ public class HelpDesk {
 
     private ArrayPriorityQueue<Ticket> customers;
     private int currID;
+    private String[] answers = {"Well, that's too bad.", "Life isn't fair, honey.", "Ain't nobody got time for that.", "EUREKA!!!", "Ask Mr. Brown.", "To be or not to be, that is the question.", "Have you utilized your Keys to Success yet?", "Ooh, girl, tell me about it.", "It's just your bad karma."};
     
     public void addTicket(int VIPLevel, String problem, String custName) {
     
         Ticket newTicket = new Ticket(VIPLevel, currID, problem, custName);
         customers.add(newTicket);
         System.out.println(custName + "needs help!");
+        
+    }
+    
+    public String solveTicket() {
+    
+        
         
     }
     
